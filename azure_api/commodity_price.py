f688f24f1bbc7d5cedb01c593d352efb443a83cf
lei0lei <lei.lei.fan.meng@gmail.com>
2023-10-11 06:54:31 +0800

    .

--- azure_api/commodity_price.py
+++ azure_api/commodity_price.py
@@ -1,10 +1,10 @@
+import requests
 
 
 
-
-def get_commodity_price():
+def get_commodity_price(code,start,end):
     endpoint = 'https://getdailycommodityprice.azurewebsites.net/api/GetDailyCommodityPrice'
     # POST: {"code":"CU",
     #         "start_date":"2023-08-01",
     #         "end_date":"2023-09-11"}
-    pass
+    return requests.post(endpoint,json = {'code':code,'start_date':start,'end_date':end}).json()
--- azure_api/future_daily_price.py
+++ azure_api/future_daily_price.py
@@ -1,10 +1,10 @@
+import requests
 
 
-
-def get_daily_future_price():
+def get_daily_future_price(code,start,end):
     endpoint = 'https://getdailyfutureprice.azurewebsites.net/api/GetDailyFuturePrice'
         # {"code":"CU2311",
         # "start_date":"2023-08-01",
         # "end_date":"2023-09-11"}
 
-    pass
+    return requests.post(endpoint,json = {'code':code,'start_date':start,'end_date':end}).json()
